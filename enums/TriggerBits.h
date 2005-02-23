25b8c360a24602b4084922849f4da7bc08226911
heather <>
2005-02-23 04:58:41 +0000

    old trigger bit definition

--- enums/TriggerBits.h
+++ enums/TriggerBits.h
@@ -5,16 +5,13 @@ namespace enums {
     typedef enum  {
         //! definition of  trigger bits
 
-        b_ACDL =     1,  ///>  set if cover or side veto, low threshold
-        b_Track=     2,  ///>  3 consecutive x-y layers hit
-        b_LO_CAL=    4,  ///>  single log above low threshold
-        b_HI_CAL=    8,  ///> single log above high threshold
-        b_ACDH =    16,  ///>  cover or side veto, high threshold ("CNO")
-        b_THROTTLE= 32,  ///> Ritz throttle
-
-        number_of_trigger_bits = 6, ///> for size of table
-
-        GEM_offset = 8  ///> offset to the GEM bits
+       // definition of  trigger bits   
+       b_ACDL =     1,  //  set if cover or side veto, low threshold   
+       b_ACDH =     2,   //  cover or side veto, high threshold   
+       b_Track=     4,   //  3 consecutive x-y layers hit   
+       b_LO_CAL=    8,  //  single log above low threshold   
+       b_HI_CAL=   16,   //  3 cal layers in a row above high threshold   
+       number_of_trigger_bits = 5 
 
     } TriggerBits;
 
