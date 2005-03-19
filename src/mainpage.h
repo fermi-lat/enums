4128669e5d4133afa33f192b3fca255df308e1e1
burnett <>
2005-03-19 19:06:49 +0000

    fix up mainpage to provide links to the bit definitions

--- src/mainpage.h
+++ src/mainpage.h
@@ -2,6 +2,10 @@
 
   @section intro Introduction
   Definition of common enumerations
+
+ - trigger bits: see TriggerBits.h
+ - Gem condition  bits: see GemConditionSummary.h
+
   <hr>
   @section notes release notes
   release.notes
