d76d08a659b14d725f4a5c14dc873708f27418c8
lsrea <>
2005-03-31 19:04:35 +0000

    Add GEM_mask to TriggerBits.h, and associated stuff

--- enums/TriggerBits.h
+++ enums/TriggerBits.h
@@ -8,17 +8,16 @@ namespace enums {
         b_ACDL =     1,  ///>  set if cover or side veto, low threshold
         b_Track=     2,  ///>  3 consecutive x-y layers hit
         b_LO_CAL=    4,  ///>  single log above low threshold
-        b_HI_CAL=    8,  ///> single log above high threshold
+        b_HI_CAL=    8,  ///>  single log above high threshold
         b_ACDH =    16,  ///>  cover or side veto, high threshold ("CNO")
-        b_THROTTLE= 32,  ///> Ritz throttle
+        b_THROTTLE= 32,  ///>  Ritz throttle
 
         number_of_trigger_bits = 6, ///> for size of table
 
-        GEM_offset = 8  ///> offset to the GEM bits
+        GEM_offset = 8,             ///> offset to the GEM bits
+        GEM_mask   = 0xff           ///> mask for GEM bits (use for trigger bits too!)
 
     } TriggerBits;
-
 }
-
 
 #endif
