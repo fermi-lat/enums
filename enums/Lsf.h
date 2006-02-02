73930da90138f7007ed9bde98a91be8bdb975609
echarles <>
2006-02-02 22:24:02 +0000

    Updated Lsf.h to match Dfi header files instead of documentation

--- enums/Lsf.h
+++ enums/Lsf.h
@@ -1,22 +1,61 @@
 #ifndef ENUMS_LSF_H
 #define ENUMS_LSF_H
 
+/*
+ * The enums in this file are take from the DFI interface and should match with  
+ * fsw/<version>/include/fsw/DFI/DfiEvent-*.hh
+ *
+ * Note that changes to this file affect the interpretation of persisted data.
+ * So don't change it.
+ *
+*/
+
 namespace enums {
+
   namespace Lsf {
+    
+    namespace Open {
+      /// The action that caused a datagram to be opened
+      typedef enum { Start,        ///> First datagram afer start of run
+		     Resume,       ///> First datagram after a run was paused and then resumed
+		     Continue,     ///> Any other datagram
+		     Unspecified,  ///> Action information was lost
+		     NumActions
+      } Action;  
 
-    /// The reason that a datagram was opened
-    typedef enum { NoOpenReason = -1, 
-		   Start=1, ///> first datagram afer start of run
-		   Resume=2 ///> first datagram after a run was paused and then resumed
-    } OpenReason;  
-
-    /// The source of the request to open this datagram
-    typedef enum { NoOpenRequest = -1, 
-		   OpenOperator, 
-		   OpenAutomatic, 
-		   OpenUnknown 
-    } OpenRequester;
-
+      /// The reason this datagram was opened
+      typedef enum { Operator,     ///>
+		     ModeChange,   ///>
+		     TimedOut,     ///>
+		     CountedOut,   ///>
+		     Full,         ///>
+		     Unknown,      ///>
+		     NumReasons    ///>
+      } Reason;
+    };
+    
+    namespace Close {
+      
+      /// The action that caused the datagram to be closed
+      typedef enum { Stop,                 ///> Last datagram in a run which stopped normally
+		     Abort,                ///> Last datagram in a run which was aborted
+		     Pause,                ///> Last datagram before a run was paused
+		     Continue,             ///> 
+		     Unspecified,          ///> Action information was lost
+		     NumActions
+      } Action;
+      
+      /// The reason this datagram was closed
+      typedef enum { Operator,     ///>
+		     ModeChange,   ///>
+		     TimedOut,     ///>
+		     CountedOut,   ///>
+		     Full,         ///>
+		     Unknown,      ///>
+		     NumReasons    ///>
+      } Reason;
+    };
+    
     /// The source of the data in this datagram
     typedef enum { NoCrate = -1, 
 		   Epu0, 
@@ -24,56 +63,44 @@ namespace enums {
 		   Epu2, 
 		   Siu0, 
 		   Siu1, 
-		   Aux 
+		   Aux,
+		   Mixed, 
+		   NumCrates
     } Crate;
-
+    
     /// The operating mode that the data in the datagram was acquired under
     typedef enum { NoMode = -1, 
 		   Normal, 
-		   TOO, ///> Target Of Oppurtunity
-		   ARR, ///> Autonomous repointing
+		   TOO,             ///> Target Of Oppurtunity
+		   ARR,             ///> Autonomous repointing
 		   Calibration, 
-		   Diagnostic 
+		   Diagnostic, 
+		   NumModes
     } Mode;
     
-    /// The reason that a datagram was closed
-    typedef enum { NoCloseReason = -1, 
-		   Stop, 
-		   Pause, 
-		   Abort, 
-		   Unknown 
-    } CloseReason;
-
-    /// The source of the request to open this datagram
-    typedef enum { NoCloseRequest = -1, 
-		   CloseOperator, 
-		   CloseAutomatic, 
-		   TimedOut, 
-		   CountedOut, 
-		   Full, 
-		   CloseUnknown 
-    } CloseRequester;
-     
     /// The platform that a run was acquired on 
     typedef enum { NoPlatform = -1, 
-		   Lat = 0, ///> The real thing
-		   Testbed = 1, ///> hardware simulation
-		   Host = 2 ///> software simulation
+		   Lat,         ///> The real thing
+		   Testbed,     ///> hardware simulation
+		   Host,        ///> software simulation
+		   NumPlatforms 
     } Platform;
-
+    
     /// The origin of this run
     typedef enum { NoOrigin = -1, 
-		   Orbit = 0, 
-		   MonteCarlo = 1, 
-		   Ground = 2 
+		   Orbit,
+		   MonteCarlo,
+		   Ground,
+		   NumOrigins
     } DataOrigin;
- 
+    
     /// The type of run.  
     typedef enum { NoRunType = -1, 
 		   LPA,    ///> Particle data
 		   AcdLCI, ///> Charge injection to Acd
 		   CalLCI, ///> Charge injection to Cal
-		   TkrLCI  ///> Charge injection to Tkr
+		   TkrLCI, ///> Charge injection to Tkr
+		   NumRunTypes
     } RunType;
 
   }//namespace lsf
