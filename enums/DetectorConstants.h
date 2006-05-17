4625f20b709962c964ca6025f0dfe264bf462cc6
heather <>
2006-05-17 06:04:26 +0000

    forgot to check this in

---
+++ enums/DetectorConstants.h
@@ -0,0 +1,17 @@
+#ifndef ENUMS_DETECTORCONSTANTS_H
+#define ENUMS_DETECTORCONSTANTS_H
+
+/*
+ *
+*/
+
+namespace enums {
+
+  typedef enum {
+      numGtcc = 8
+  } DetectorConstants;
+
+}//namespace enums
+
+
+#endif
