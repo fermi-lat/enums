4aba784acef6be2d58646e98faacac4f46e07b82
heather <>
2006-06-05 17:08:52 +0000

    OBF constants

---
+++ enums/EfcGammaStatus.h
@@ -0,0 +1,18 @@
+#ifndef ENUMS_EFCGAMMASTATUS_H
+#define ENUMS_EFCGAMMASTATUS_H
+
+namespace enums {
+
+
+typedef enum _EFC_GAMMA_STAGE_M
+{
+   EFC_GAMMA_STAGE_M_ENERGY         = 0xffffff
+   /*!< The energy portion of the stage and energy result word            */
+
+
+}
+EFC_GAMMA_STAGE_M;
+
+} // end enums namespace
+
+#endif
