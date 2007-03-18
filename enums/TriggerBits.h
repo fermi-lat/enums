262b69505315e395788edd6293f1678d9acda7de
burnett <>
2007-03-18 16:27:29 +0000

    move acdl bit to 64; tagging

--- enums/TriggerBits.h
+++ enums/TriggerBits.h
@@ -5,16 +5,15 @@ namespace enums {
     typedef enum  {
         //! definition of  trigger bits
 
-        b_ACDL =     1,  ///>  set if cover or side veto, low threshold
-        b_ROI =      1,  ///<  copy of throttle,  if (new style) mimic GEM word
-
+        b_ROI =      1,  ///>  throttle
         b_Track=     2,  ///>  3 consecutive x-y layers hit
         b_LO_CAL=    4,  ///>  single log above low threshold
         b_HI_CAL=    8,  ///>  single log above high threshold
         b_ACDH =    16,  ///>  cover or side veto, high threshold ("CNO")
-        b_THROTTLE= 32,  ///>  Ritz throttle
+        b_THROTTLE= 32,  ///>  Ritz throttle (now redundant)
+        b_ACDL =    64,  ///>  set if cover or side veto, low threshold
 
-        number_of_trigger_bits = 6, ///> for size of table
+        number_of_trigger_bits = 7, ///> for size of table
 
         GEM_offset = 8,             ///> offset to the GEM bits
         GEM_mask   = 0xff           ///> mask for GEM bits (use for trigger bits too!)
