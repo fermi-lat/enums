e4d7fe7b59e3bc79b7601e288afcc525c61d2f2f
burnett <>
2007-10-20 21:54:16 +0000

    removed non-trigger bit ACDL

--- enums/TriggerBits.h
+++ enums/TriggerBits.h
@@ -10,17 +10,18 @@ namespace enums {
         b_LO_CAL=    4,  ///>  single log above low threshold
         b_HI_CAL=    8,  ///>  single log above high threshold
         b_ACDH =    16,  ///>  cover or side veto, high threshold ("CNO")
-        b_THROTTLE= 32,  ///>  Ritz throttle (now redundant)
+#if 0 // no longer needed
         b_ACDL =    64,  ///>  set if cover or side veto, low threshold
+#endif 
 
-        number_of_trigger_bits = 7, ///> for size of table
+        number_of_trigger_bits = 5, ///> for size of table
 
         GEM_offset = 8,             ///> offset to the GEM bits
         GEM_mask   = 0xff,           ///> mask for GEM bits (use for trigger bits too!)
 
-		ENGINE_offset = 5,          /// number of bits assigned for engine numbers
-		ENGINE_mask = 0x1f,         /// mask for engine numbers
-		ENGINE_unset = 16          /// Signifies that the engine is unset, they are normally in the range [0,15]
+        ENGINE_offset = 5,          /// number of bits assigned for engine numbers
+        ENGINE_mask = 0x1f,         /// mask for engine numbers
+        ENGINE_unset = 16          /// Signifies that the engine is unset, they are normally in the range [0,15]
 
     } TriggerBits;
 }
