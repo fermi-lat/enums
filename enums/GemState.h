8c80ef50a166e559d84d6f7ba75654f3256ccc74
kocian <>
2007-11-02 18:59:37 +0000

    GEM states

---
+++ enums/GemState.h
@@ -0,0 +1,17 @@
+#ifndef ENUMS_GEMSTATE_H
+#define ENUMS_GEMSTATE_H
+
+namespace enums {
+    
+    /// Defines 3 stqtes of the GEM 
+    typedef enum {
+            LIVE=0,
+            DEADZONE=1,
+            BUSY=2
+    } GemState;
+
+
+}
+
+
+#endif
